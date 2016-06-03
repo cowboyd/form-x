16ff6cb46ac50f3e765509bc6240ba6e1e7c42fb
Charles Lowell <cowboyd@frontside.io>
2016-06-03 13:12:20 -0500

    add states for no rules

--- src/validation.js
+++ src/validation.js
@@ -1,6 +1,35 @@
 export default class Validation {
 
-  static create() {
-    return new this();
+  static create(options = {}) {
+    let {
+      isRequired = false,
+      rules = []
+    } = options;
+    return new IdleValidation({rules, isRequired});
   }
+
+  constructor(attrs = {}, overrides = {}) {
+    Object.assign(this, {
+      isRequired: false
+    }, attrs, overrides);
+  }
+
+  get isIdle() { return false; }
+  get isPending() { return false; }
+  get isSatisfied() { return false; }
+
+
+  setInput(input) {
+    return new SatisfiedValidation(this, {input});
+  }
+}
+
+class IdleValidation extends Validation {
+  get isIdle() { return true; }
+  get isSatisfied() { return !this.isRequired; }
+}
+
+class SatisfiedValidation extends Validation {
+  get isIdle() { return false; }
+  get isSatisfied() { return true; }
 }
