5226748a88597f09bc3b044458b18402209c8c16
Jeff Felchner <accounts+git@thekompanee.com>
2016-06-03 17:15:50 -0500

    Start fresh with Validation and handle initial states

--- src/validation.js
+++ src/validation.js
@@ -1,35 +1,20 @@
 export default class Validation {
 
-  static create(options = {}) {
-    let {
-      isRequired = false,
-      rules = []
-    } = options;
-    return new IdleValidation({rules, isRequired});
+  static create(_options = {}) {
+    return new IdleValidation();
   }
 
   constructor(attrs = {}, overrides = {}) {
     Object.assign(this, {
-      isRequired: false
     }, attrs, overrides);
   }
 
   get isIdle() { return false; }
   get isPending() { return false; }
-  get isSatisfied() { return false; }
-
-
-  setInput(input) {
-    return new SatisfiedValidation(this, {input});
-  }
+  get isFulfilled() { return false; }
+  get isRejected() { return false; }
 }
 
 class IdleValidation extends Validation {
   get isIdle() { return true; }
-  get isSatisfied() { return !this.isRequired; }
-}
-
-class SatisfiedValidation extends Validation {
-  get isIdle() { return false; }
-  get isSatisfied() { return true; }
 }
--- test/validation-test.js
+++ test/validation-test.js
@@ -1,104 +1,26 @@
-import { describe, beforeEach, it } from 'mocha';
-import { expect } from 'chai';
-import Validation  from '../src/validation';
+import Validation  from "../src/validation";
+import { expect } from "chai";
 
-// describe("Validation", function() {
-//   let state;
+/* eslint-disable max-nested-callbacks, func-names, prefer-arrow-callback */
+describe("Validation", function() {
+  let validation = null;
 
-//   describe("when empty", function() {
-//     beforeEach(function() {
-//       state = Validation.create();
-//     });
-//     it("is idle", function() {
-//       expect(state.isIdle).to.equal(true);
-//     });
-//     it("is not required by default", function() {
-//       expect(state.isRequired).to.equal(false);
-//     });
-//     it("is satisfied becauce it is not required", function() {
-//       expect(state.isSatisfied).to.equal(true);
-//     });
-//     it("has rules", function() {
-//       expect(state.all).to.deep.equal([]);
-//       expect(state.pending).to.deep.equal([]);
-//     });
-//     describe("setting any input whatsoever", function() {
-//       beforeEach(function() {
-//         state = state.setInput('wut?');
-//       });
-//       it("is not idle", function() {
-//         expect(state.isIdle).to.equal(false);
-//       });
-//       it("is not pending", function() {
-//         expect(state.isPending).to.equal(false);
-//       });
-//       it("is satisfied", function() {
-//         expect(state.isSatisfied).to.equal(true);
-//       });
-//     });
-//   });
+  describe("when empty", function() {
+    beforeEach(function() {
+      validation = Validation.create();
+    });
 
-//   describe("without rules, but required", function() {
-//     beforeEach(function() {
-//       state = Validation.create({isRequired: true});
-//     });
-//     it("is not satisfied", function() {
-//       expect(state.isSatisfied).to.equal(false);
-//     });
-//     describe("setting any input what", function() {
-//       beforeEach(function() {
-//         state = state.setInput('hallo');
-//       });
-//       it("is satisfied", function() {
-//         expect(state.isSatisfied).to.equal(true);
-//       });
-//     });
-//   });
-
-//   describe("with some password rules", function() {
-//     let server, resolve, reject;
-
-//     beforeEach(function() {
-//       server = new Promise(function() {
-//         [resolve, reject] = arguments;
-//       });
-//       state = Validation.create({
-//         rules: {
-//           longEnough: {
-//             check: function(value, assert) {
-//               assert("must be longer than 5 characters", value.length > 0);
-//             }
-//           },
-//           hasSpecialCharacter: {
-//             check: function(value, assert) {
-//               assert("must contain at least one special character", );
-//             }
-//           },
-//           unique: {
-//             check: function(value, assert) {
-//               return new Promise(function(resolve, reject) {
-
-//               });
-//             },
-//             dependsOn: ['longEnough', 'hasSpecialCharacter']
-//           }
-//         }
-//       });
-//     });
-
-//     it("isIdle", function() {
-//       expect(state.isIdle).to.equal(true);
-//     });
-//     it("is not required by default", function() {
-//       expect(state.isRequired).to.equal(false);
-//     });
-//     it("is satisfied because it is not required", function() {
-//       expect(state.isSatisfied).to.equal(true);
-//     });
-//     describe("setting some valid input", function() {
-//       beforeEach(function() {
-//         state = state.setInput('$bob$');
-//       });
-//     });
-//   });
-// });
+    it("is idle", function() {
+      expect(validation.isIdle).to.equal(true);
+    });
+    it("is not pending", function() {
+      expect(validation.isPending).to.equal(false);
+    });
+    it("is not fulfilled", function() {
+      expect(validation.isFulfilled).to.equal(false);
+    });
+    it("is not rejected", function() {
+      expect(validation.isRejected).to.equal(false);
+    });
+  });
+});
